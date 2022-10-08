faaa81868e4b89afa52786d516427f1e1c5ed281
ahnseoYoo <ahnseo.yoo@gmail.com>
2022-10-09 06:08:52 +0900

    자동형변환 때문에 앞뒤 버튼 오류 발생했었음. 수정 완료.

--- src/posts.js
+++ src/posts.js
@@ -7,43 +7,43 @@ import Post from "./post.js"
 const Posts = () => {
 
 
-    let postPerPage = 10;
-    let postCount = agoraStatesDiscussions.length;
-    let pageCount = Math.ceil(postCount/postPerPage);
-    let pages = [];
-    for (let i = 1; i < pageCount + 1; i++) {
+    let postPerPage = 10; // 한 페이지에 표시할 글 수
+    let postCount = agoraStatesDiscussions.length; // 모든 글 수
+    let pageCount = Math.ceil(postCount/postPerPage); // 페이지 수
+    let pages = []; // 페이지 아이콘을 렌더링 하기 위한 배열
+    for (let i = 1; i < pageCount + 1; i++) { // 페이지 아이콘 배열에 페이지 넘버 푸쉬
         pages.push(i);
     }
-    const[pageState,setPageState] = useState(1);
-    const[frontBack, setFrontBack] = useState(null);
+
+
+    const[pageState,setPageState] = useState(1); //페이지 버튼 뭐 눌렀는지 상태. 디폴트는 1페이지.
 
     const handlePageClick = event => {
-        setPageState(event.target.textContent);
+        setPageState(event.target.textContent);// 버튼 숫자에 따라 state 바뀜
     } 
 
     const frontOrBack = event => {
-        if(event.target.textContent === "<" && pageState > 1){
-            setPageState(pageState - 1);
+
+        if(event.target.textContent === ">" && pageState < pages.length){
+            setPageState(Number(pageState) + 1); //아니 pageState 맨처음에 그냥 Num타입으로 줬는데 스트링으로 변환됨;; 자동 형변환 킹받네 ㄹㅇ 아무튼 그래서 Number()로 재변환 해줌
         }
-        else if(event.target.textContent === ">" && pageState < pages.length){
-            setPageState(pageState + 1);
+        else if(event.target.textContent === "<" && pageState > 1){
+            setPageState(Number(pageState) - 1);
         }
     }
    
 
-
     return (
         <div>
-            {pageState?
+            {pageState? //클릭한 페이지에 따른 조건부 렌더링
                 (<div className = "posts">{agoraStatesDiscussions.slice((pageState-1)*postPerPage, pageState * postPerPage).map((post) => <Post data = {post}/>)}
                 {`total post count: ${agoraStatesDiscussions.length}`}</div>)
                 :null
-
             }
-            <div className='pageIcons'>
-                <button className={`button<`} onClick={frontOrBack}>{"<"}</button>
-                {pages.map((element) => <button className={`button${element}`} onClick={handlePageClick}>{element}</button>)}
-                <button className={`button>`} onClick={frontOrBack}>{">"}</button>
+            <div className='pageIcons'> {/*페이지 아이콘*/}
+                <button className={`button<`} onClick={frontOrBack}>{"<"}</button> {/*뒤로가기*/}
+                {pages.map((element) => <button className={`button${element}`} onClick={handlePageClick}>{element}</button>)} {/*숫자*/}
+                <button className={`button>`} onClick={frontOrBack}>{">"}</button> {/*앞으로가기*/}
 
             </div>
         </div>  
